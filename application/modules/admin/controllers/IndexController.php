ec20bda05018f5e150f1bdbeeee51fec17ab75d0
ChiNTK <nguyen.thi.kim.chi@framgia.com>
2015-04-03 16:35:19 +0700

    Manager user

--- application/modules/admin/controllers/IndexController.php
+++ application/modules/admin/controllers/IndexController.php
@@ -1,36 +1,6 @@
 <?php
-class Admin_IndexController extends Zend_Controller_Action{
-  public function init(){    
-    $this->view->headTitle("Admin");
-  }
-
+require_once 'AuthController.php';
+class Admin_IndexController extends Admin_AuthController{
   public function indexAction(){
-  }     
-  
-  public function preDispatch(){
-    $auth = Zend_Auth::getInstance();
-    if($auth->hasIdentity()){
-      $acl = Zend_Registry::get('acl');
-      $role="";
-      switch($auth->getIdentity()->type){
-        case 1: $role = "admin"; break;
-        case 2: $role = "master"; break;
-        case 3: $role = "expert"; break;
-        case 4: $role = "leader"; break;
-        case 5: $role = "staff"; break;
-        case 6: $role = "user"; break;
-        default : $role = "user";
-      }
-      $module = $this->_request->getModuleName();
-      $controller = $this->_request->getControllerName();
-      $action =$this->_request->getActionName();
-      $resource=$module.":".$controller;
-      if(!$acl->isAllowed($role,$resource,$action)){
-        $this->_redirect('/index/error');
-      }
-      $this->view->user = $auth->getIdentity();     
-    }else{
-      $this->_redirect('/index/login');
-    }
   }
 }
--- application/modules/default/controllers/IndexController.php
+++ application/modules/default/controllers/IndexController.php
@@ -7,7 +7,7 @@ class IndexController extends Zend_Controller_Action{
   public function preDispatch(){ 
     $auth = Zend_Auth::getInstance();
     if($auth->hasIdentity()){
-      switch($auth->getIdentity()->type){
+      switch($auth->getIdentity()->level){
         case 1: $role = "admin"; break;
         case 2: $role = "master"; break;
         case 3: $role = "expert"; break;
@@ -37,34 +37,42 @@ class IndexController extends Zend_Controller_Action{
   }
 
   public function loginAction(){
-    $this->view->headTitle(" | Login");  
-    $mod=$this->_request->getModuleName();
-    $formLogin = new Form_Login();
-    $this->view->formLogin = $formLogin;
-    if($this->_request->getPost('login')){
-      if($formLogin->isValid($this->_request->getPost())){
-        $db = Zend_Db_Table::getDefaultAdapter();
-        $authAdapter = new Zend_Auth_Adapter_DbTable($db);
-        $authAdapter->setTableName('user')
-                    ->setIdentityColumn('email')
-                    ->setCredentialColumn('password');
-        $request = $this->getRequest();
-        $uname = $request->getParam('email');
-        $paswd = $request->getParam('password');
-        $authAdapter->setIdentity($uname);
-        $authAdapter->setCredential(md5($paswd));
-        $select = $authAdapter->getDbSelect();
-        $auth = Zend_Auth::getInstance();
-        $result = $auth->authenticate($authAdapter);
-        if($result->isValid()){
-          $getInfo = $authAdapter->getResultRowObject();
-          $auth->getStorage()->write($getInfo);
-          if($getInfo->type == 1)
-            $this->_redirect('/admin/index/index');
-          else
-            $this->_redirect('/');
-        }else{
-          $this->_redirect('/index/login');   
+    $auth = Zend_Auth::getInstance();
+    if($auth->hasIdentity()){
+      if($getInfo->level == 1)
+        $this->_redirect('/admin/index/index');
+      else
+        $this->_redirect('/');
+    }else{
+      $this->view->headTitle(" | Login");  
+      $mod=$this->_request->getModuleName();
+      $formLogin = new Form_Login();
+      $this->view->formLogin = $formLogin;
+      if($this->_request->getPost('login')){
+        if($formLogin->isValid($this->_request->getPost())){
+          $db = Zend_Db_Table::getDefaultAdapter();
+          $authAdapter = new Zend_Auth_Adapter_DbTable($db);
+          $authAdapter->setTableName('user')
+                      ->setIdentityColumn('email')
+                      ->setCredentialColumn('password');
+          $request = $this->getRequest();
+          $uname = $request->getParam('email');
+          $paswd = $request->getParam('password');
+          $authAdapter->setIdentity($uname);
+          $authAdapter->setCredential(md5($paswd));
+          $select = $authAdapter->getDbSelect();
+          $auth = Zend_Auth::getInstance();
+          $result = $auth->authenticate($authAdapter);
+          if($result->isValid()){
+            $getInfo = $authAdapter->getResultRowObject();
+            $auth->getStorage()->write($getInfo);
+            if($getInfo->level == 1)
+              $this->_redirect('/admin/index/index');
+            else
+              $this->_redirect('/');
+          }else{
+            $this->_redirect('/index/login');   
+          }
         }
       }
     }
